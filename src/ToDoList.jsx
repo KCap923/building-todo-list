f13b3a179987bc9ba752be5f42d08efc92a47873
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-15 23:42:42 -0400

    CSS

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,77 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function ToDoList(){
+function ToDoList() {
+  const [tasks, setTasks] = useState([
+    "Learn HTML",
+    "Learn CSS",
+    "Learn Javascript",
+    "Attend Bootcamp",
+    "Finish Bootcamp Strong!",
+    "Master Skills",
+    "Take  DSA Course",
+    "React Native",
+    "Learn Python",
+  ]);
+  const [newTask, setNewTask] = useState("");
 
-const [ tasks, setTasks ] = useState(["Learn HTML", "Learn CSS", "Learn Javascript", "Attend Bootcamp", "Finish Bootcamp Strong!", "Master Skills", "Take  DSA Course", "React Native", "Learn Python",]);
-const [ newTask, setNewTask ] = useState("");
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+  function addTask() {}
+  function deleteTask(index) {}
+  function editTask(index) {}
 
-function handleInputChange(event) {
-setNewTask(event.target.value);
+  return (
+    <div className="to-do-list">
+      <h1> To-Do List </h1>
 
-}
-function addTask() {
+      <div>
+        <input
+          type="text"
+          placeholder="Add task ..."
+          value={newTask}
+          onChange={handleInputChange}/>
 
+        <button className="add-button"> Add </button>
+        <button className="edit-button"> Edit </button>
+      </div>
 
-}
-function deleteTask(index) {
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span> 
 
+{/* Checkbox ***************************************************************************************/}
+            <input type="checkbox" className="check-box" id="check-box" />
 
-}
-function moveTaskUp(index) {
+{/* Edit Button ***************************************************************************************/}
+            <button className="edit-button" onClick={() => editTask(index)}>
+              Edit
+            </button>
 
+{/* Delete Button ***************************************************************************************/}
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
 
-}
-function moveTaskDown(index) {
-
-
-}
-
-return (
-
-<div className="to-do-list">
-
-  <h1> To-Do List </h1> 
-
-  <div> 
-    
-    <input 
-    type="text"
-    placeholder='Enter a task'
-    value={newTask}
-    onChange={handleInputChange} />
-
-    <button className="add-button">
-      Add
-    </button>
-
-
-    
-    <button className="edit-button">
-      Edit
-    </button>
-    
-  </div>
-
-<ol>
-
-  {tasks.map((task, index) => 
-  <li key={index}> 
-  <span className="text">{task}</span>
-
-  <button className="delete-button">
-      Delete
-    </button>
-  </li>
-  
-  )}
-
-</ol>
-
-</div>
-
-
-);
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-
-export default ToDoList
+export default ToDoList;
