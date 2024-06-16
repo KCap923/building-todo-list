321f9f89e9aba6058db2891c5efda7d4c5908f23
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-15 21:25:20 -0400

    Buttons

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -6,7 +6,7 @@ const [ tasks, setTasks ] = useState([]);
 const [ newTask, setNewTask ] = useState("");
 
 function handleInputChange(event) {
-
+setNewTask(event.target.value);
 
 }
 function addTask() {
@@ -23,14 +23,40 @@ function moveTaskUp(index) {
 }
 function moveTaskDown(index) {
 
-  
-}
-
-
-return (<></>);
-
-
 
 }
+
+return (
+
+<div className="to-do-list">
+
+  <h1> To-Do List </h1> 
+
+  <div> 
+    
+    <input 
+    type="text"
+    placeholder='Enter a task'
+    value={newTask}
+    onChange={handleInputChange} />
+
+    <button className="addbutton">
+      Add
+    </button>
+    <button className="delete">
+      Delete
+    </button>
+    <button className="edit">
+      Edit
+    </button>
+    
+  </div>
+
+</div>
+
+
+);
+}
+
 
 export default ToDoList
